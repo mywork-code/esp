d4cff352124a30e215b65b08c9eca87e472440d7
wangxianzhi <wangxianzhi@apass.com>
2017-04-21 17:03:45 +0800

    Revert "update"
    This reverts commit 522edc982ab2f836558fc33683112832751996dd.

--- esp-appweb/src/main/java/com/apass/esp/web/activity/ActivityWithDrawController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/activity/ActivityWithDrawController.java
@@ -1,19 +1,14 @@
 package com.apass.esp.web.activity;
 
 import java.util.Date;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.apass.esp.domain.Response;
 import com.apass.esp.domain.entity.AwardActivityInfo;
-import com.apass.esp.domain.enums.AwardActivity;
 import com.apass.esp.service.activity.AwardActivityInfoService;
-import com.apass.gfb.framework.utils.CommonUtils;
-import com.apass.gfb.framework.utils.GsonUtils;
 
 @Controller
 @RequestMapping("activity/award")
@@ -30,69 +25,7 @@ public class ActivityWithDrawController {
 		obj.setType((byte) 0);
 		obj.setaStartDate(new Date());
 		long obj1 = awardActivityInfoService.addActivity(obj);
-		return obj1 + "";
-
-	}
-
-	/**
-	 * 查询是否绑定银行卡及卡片信息
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/getBindCardImformation")
-	@ResponseBody
-	public Response getBindCardImformation(Map<String, Object> paramMap) {
-		String userId = CommonUtils.getValue(paramMap, "userId");
-		String requestId = AwardActivity.AWARD_ACTIVITY_METHOD.BINDCARD.getCode() + "_" + userId;
-		String result = awardActivityInfoService.getBindCardImformation(requestId, Long.valueOf(userId));
-		if (result == null) {
-			return Response.fail(userId);
-		} else if (result.equals(AwardActivity.BIND_STATUS.UNBINDED.getCode())) {
-			return Response.successResponse("unbind");
-		} else {
-			return Response.successResponse(result);
-		}
-	}
-
-	/**
-	 * 绑定卡片
-	 */
-	public Response bindCardByUserId(Map<String, Object> paramMap) {
-
-		String userId = CommonUtils.getValue(paramMap, "userId");
-		String identityNo = CommonUtils.getValue(paramMap, "identityNo");
-		String realName = CommonUtils.getValue(paramMap, "realName");
-		String cardNo = CommonUtils.getValue(paramMap, "cardNo");
-		String mobile = CommonUtils.getValue(paramMap, "mobile");
-		// 校验
-
-		String requestId = AwardActivity.AWARD_ACTIVITY_METHOD.BINDCARD.getCode() + "_" + userId;
-		String result = awardActivityInfoService.getBindCardImformation(requestId, Long.valueOf(userId));
-		if (result == null) {
-			return Response.fail("对不起,该用户不存在!");
-		}
-		Map<String, Object> map = GsonUtils.convert(result);
-		if (map.containsKey("status") && "0".equals(map.get("status"))) {
-			return Response.fail("对不起,该用户已经绑定银行卡");
-		}
-		// 客户存在且未绑定银行卡
-
-		paramMap.put("customerId", map.get("customerId"));
-
-		// 验卡是否本人 以及是否支持该银行
-
-		Response res = awardActivityInfoService.validateBindCard(paramMap);
-		if (!"1".equals(res.getStatus())) {
-			return Response.fail(userId);
-			// Map resMap = GsonUtils.convert((String) res.getData());
-		}
-		// 绑卡
-
-		Response response = awardActivityInfoService.bindCard(paramMap);
-		if (!"1".equals(response.getStatus())) {
-			return Response.fail(userId);
-		}
-		return Response.success("success");
+		return obj1+"";
 
 	}
 
--- esp-service/src/main/java/com/apass/esp/service/activity/AwardActivityInfoService.java
+++ esp-service/src/main/java/com/apass/esp/service/activity/AwardActivityInfoService.java
@@ -1,22 +1,14 @@
 package com.apass.esp.service.activity;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.apass.esp.domain.Response;
 import com.apass.esp.domain.entity.AwardActivityInfo;
-import com.apass.esp.domain.entity.customer.CustomerInfo;
-import com.apass.esp.domain.enums.AwardActivity;
 import com.apass.esp.mapper.AwardActivityInfoMapper;
-import com.apass.esp.repository.payment.PaymentHttpClient;
-import com.apass.gfb.framework.exception.BusinessException;
-import com.apass.gfb.framework.utils.GsonUtils;
+import com.apass.esp.mapper.AwardBindRelMapper;
+import com.apass.esp.mapper.AwardDetailMapper;
 
 @Service
 public class AwardActivityInfoService {
@@ -25,70 +17,9 @@ public class AwardActivityInfoService {
 	@Autowired
 	public AwardActivityInfoMapper awardActivityInfoMapper;
 
-	@Autowired
-	private PaymentHttpClient paymentHttpClient;
-
-	/**
-	 * 添加活动
-	 * 
-	 * @param awardActivityInfo
-	 * @return
-	 */
 	public long addActivity(AwardActivityInfo awardActivityInfo) {
 		awardActivityInfoMapper.insert(awardActivityInfo);
 		return awardActivityInfo.getId();
-	}
-
-	/**
-	 * 查询用户是否绑卡及绑卡信息
-	 * 
-	 * @param userId
-	 * @return
-	 */
-	public String getBindCardImformation(String requestId, long userId) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		try {
-			CustomerInfo customerInfo = paymentHttpClient.getCustomerInfo(requestId, userId);
-			// 客户信息不存在
-			if (customerInfo == null) {
-				return null;
-			}
-			// 未绑定
-			if (StringUtils.isAnyEmpty(customerInfo.getBankCode(), customerInfo.getCardBank(),
-					customerInfo.getCardType(), customerInfo.getCardNo())) {
-				resultMap.put("userId", userId);
-				resultMap.put("customerId", customerInfo.getCustomerId());
-				resultMap.put("status", AwardActivity.BIND_STATUS.UNBINDED.getCode());
-				return GsonUtils.toJson(resultMap);
-			}
-			resultMap.put("status", AwardActivity.BIND_STATUS.BINDED.getCode());
-			resultMap.put("userId", userId);
-			resultMap.put("customerId", customerInfo.getCustomerId());
-			resultMap.put("cardType", customerInfo.getCardType());
-			resultMap.put("cardNo", customerInfo.getCardNo());
-			resultMap.put("cardBank", customerInfo.getCardBank());
-			resultMap.put("bankCode", customerInfo.getBankCode());
-			return GsonUtils.toJson(resultMap);
-		} catch (BusinessException e) {
-			return null;
-		}
-	}
-
-	/**
-	 * 验卡是否本人 以及是否支持该银行
-	 * 
-	 * @param map
-	 * @return
-	 */
-	public Response validateBindCard(Map<String, Object> map) {
-		Response res = paymentHttpClient.validateBindCard(map);
-		return res;
-	}
-
-	// 绑卡
-	public Response bindCard(Map<String, Object> map) {
-		Response res = paymentHttpClient.bindCard(map);
-		return res;
 	}
 
 }
