c00f16d5ef52fd99c2703b7a2f70a87afcf0e70a
xujie <xujie@apass.cn>
2017-04-21 13:29:17 +0800

    add mysqlTransactionManager

--- esp-framework/src/main/java/com/apass/gfb/framework/mybatis/DataBaseConfiguration.java
+++ esp-framework/src/main/java/com/apass/gfb/framework/mybatis/DataBaseConfiguration.java
@@ -3,10 +3,13 @@ package com.apass.gfb.framework.mybatis;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -41,6 +44,11 @@ public class DataBaseConfiguration {
     return this.createEnvDataSource();
   }// mysql只需要配置数据源即可，sqlsessionFactory和事务已经在app包下配置，同时mybatis也已经在app包中配置
 
+  @Bean(name = "mysqlTransactionManager")
+  @Primary
+  public PlatformTransactionManager mysqlTransactionManager(@Qualifier("mysqlDataSource") DataSource mysqlDataSource) {
+    return new DataSourceTransactionManager(mysqlDataSource);
+  }
 
   private DruidDataSource createEnvDataSource() {
     DruidDataSource datasource = new DruidDataSource();
