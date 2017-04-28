9243e770c6407cc2107cd9475fbf116191e45810
wangxianzhi <wangxianzhi@apass.com>
2017-04-28 15:33:40 +0800

    update

--- esp-appweb/src/main/java/com/apass/esp/schedule/RebateActivityScheduleTask.java
+++ esp-appweb/src/main/java/com/apass/esp/schedule/RebateActivityScheduleTask.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
