f6c4ef748e11a34ffdf127417d07cbf0c8490012
wangxianzhi <wangxianzhi@apass.com>
2017-05-04 13:26:57 +0800

    修改

--- esp-appweb/src/main/java/com/apass/esp/nothing/GoodsBasicController.java
+++ esp-appweb/src/main/java/com/apass/esp/nothing/GoodsBasicController.java
@@ -1,6 +1,8 @@
 package com.apass.esp.nothing;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apass.esp.domain.Response;
+import com.apass.esp.domain.entity.goods.GoodsInfoEntity;
+import com.apass.esp.domain.entity.goods.GoodsStockInfoEntity;
+import com.apass.esp.service.common.CommonService;
 import com.apass.esp.service.goods.GoodsService;
 import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.utils.BaseConstants.ParamsCode;
 import com.apass.gfb.framework.utils.CommonUtils;
+import com.apass.gfb.framework.utils.EncodeUtils;
 
 @RestController
 @RequestMapping("GoodsBasic")
@@ -25,6 +31,9 @@ public class GoodsBasicController {
 	@Autowired
 	private GoodsService goodService;
 
+	@Autowired
+	private CommonService commonService;
+
 	/**
 	 * 获取商品详细信息 基本信息详细信息(规格 价格 剩余量)
 	 * 
@@ -32,20 +41,30 @@ public class GoodsBasicController {
 	 */
 	@RequestMapping("/loadGoodsBasicInfoWithOutUserId")
 	public Response loadGoodsBasicInfoWithOutUserId(@RequestBody Map<String, Object> paramMap) {
-		try {
-			String goodsId = CommonUtils.getValue(paramMap, ParamsCode.GOODS_ID);
 
+		try {
 			Map<String, Object> returnMap = new HashMap<>();
-			if (StringUtils.isEmpty(goodsId)) {
+			Long goodsId = CommonUtils.getLong(paramMap, "goodsId");
+			if (null == goodsId) {
 				return Response.fail("商品号不能为空!");
 			}
-			goodService.loadGoodsBasicInfoById(Long.valueOf(goodsId), returnMap);
+			List<GoodsStockInfoEntity> goodsStockList = goodService.loadDetailInfoByGoodsId(goodsId);
+			for (GoodsStockInfoEntity goodsStock : goodsStockList) {
+				BigDecimal price = commonService.calculateGoodsPrice(goodsStock.getGoodsId(), goodsStock.getId());
+				goodsStock.setGoodsPrice(price);
+				goodsStock.setStockLogo(EncodeUtils.base64Encode(goodsStock.getStockLogo()));
+			}
+			GoodsInfoEntity goodsInfo = goodService.selectByGoodsId(goodsId);
+			if (null != goodsInfo) {
+				returnMap.put("skyType", goodsInfo.getGoodsSkuType());
+			}
+			returnMap.put("goodsStockList", goodsStockList);
 			return Response.success("加载成功", returnMap);
 		} catch (BusinessException e) {
-			LOGGER.error("ShopHomeController loadGoodsBasicInfo fail", e);
+			LOGGER.error("GoodsBasic loadGoodsBasicInfo fail", e);
 			return Response.fail(e.getErrorDesc());
 		} catch (Exception e) {
-			LOGGER.error("ShopHomeController loadGoodsBasicInfo fail", e);
+			LOGGER.error("GoodsBasic loadGoodsBasicInfo fail", e);
 			return Response.fail("获取商品基本信息失败");
 		}
 	}
