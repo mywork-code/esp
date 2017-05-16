648632c63ab9eaf9c314f620ddd8f96ecac15c80
pengyingchao <pengyingchao@apass.cn>
2017-05-16 11:56:29 +0800

    添加删除分类验证，添加上传文件

--- esp-service/src/main/java/com/apass/esp/mapper/CategoryMapper.java
+++ esp-service/src/main/java/com/apass/esp/mapper/CategoryMapper.java
@@ -3,6 +3,8 @@ package com.apass.esp.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.apass.esp.common.model.QueryParams;
 import com.apass.esp.domain.entity.Category;
 import com.apass.gfb.framework.mybatis.GenericMapper;
@@ -11,9 +13,9 @@ public interface CategoryMapper extends GenericMapper<Category, Long> {
     
     List<Category> pageEffectiveList(QueryParams query);
     
-    List<Category> selectByParentKey(Long parentId);
+    List<Category> selectByParentKey(@Param("parentId") Long parentId);
     
-    List<Category> selectByCategoryName(String categoryName);
+    List<Category> selectByCategoryName(@Param("categoryName") String categoryName);
     
 }
 
