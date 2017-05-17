bc811dfe6d2dd66f07170d9c28f2b7f20c802080
wangxianzhi <wangxianzhi@apass.com>
2017-05-17 10:33:40 +0800

    商户ID

--- esp-service/src/main/java/com/apass/esp/service/goods/GoodsService.java
+++ esp-service/src/main/java/com/apass/esp/service/goods/GoodsService.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import com.apass.esp.domain.entity.merchant.MerchantInfoEntity;
 import com.apass.esp.service.common.ImageService;
+import com.apass.esp.service.merchant.MerchantInforService;
 import com.apass.gfb.framework.utils.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -45,6 +47,9 @@ public class GoodsService {
     private CommonService            commonService;
     @Autowired
     private ImageService             imageService;
+    @Autowired
+    private MerchantInforService merchantInforService;
+
     @Autowired
     private GoodsBasicRepository     goodsBasicRepository;
     /**
@@ -258,12 +263,22 @@ public class GoodsService {
         //商品编号
         StringBuffer sb = new StringBuffer();
         String merchantCode  =entity.getMerchantCode();
-        merchantCode = merchantCode.substring(merchantCode.length()-2,merchantCode.length());
-        sb.append(merchantCode);
-        String random = RandomUtils.getRandomNum(6);
-        sb.append(random);
-        entity.setGoodsCode(sb.toString());
-        goodsDao.updateGoods(entity);
+        MerchantInfoEntity merchantInfoEntity =  merchantInforService.queryByMerchantCode(merchantCode);
+        if(merchantInfoEntity!=null){
+           String merchantId =  String.valueOf(merchantInfoEntity.getId());
+           if(merchantId.length()==1){
+               merchantId="0"+merchantId;
+           }else if(merchantId.length()>1){
+               merchantId = merchantId.substring(merchantId.length()-2,merchantId.length());
+           }
+            sb.append(merchantId);
+            String random = RandomUtils.getRandomNum(6);
+            sb.append(random);
+            entity.setGoodsCode(sb.toString());
+            goodsDao.updateGoods(entity);
+        }
+
+
     }
 
     /**
