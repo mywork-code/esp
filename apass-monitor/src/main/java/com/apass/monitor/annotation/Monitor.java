08790071491c0bef49d2e66d78ed372ce4d73ad8
xujie <xujie@apass.cn>
2017-05-18 14:07:28 +0800

    annotation

--- apass-monitor/src/main/java/com/apass/monitor/annotation/Monitor.java
+++ apass-monitor/src/main/java/com/apass/monitor/annotation/Monitor.java
@@ -1,7 +1,18 @@
 package com.apass.monitor.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by jie.xu on 17/5/18.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Monitor {
+
+  String methodMemo() default "";//方法备注
+
+
 }
