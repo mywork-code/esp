1dd797778819c9afef523f40ef7d302e1e86ee04
wangxianzhi <wangxianzhi@apass.com>
2017-05-18 15:40:34 +0800

    monitor mapper controller

---
+++ esp-ams/src/main/java/com/apass/esp/web/monitor/MonitorController.java
@@ -0,0 +1,30 @@
+package com.apass.esp.web.monitor;
+
+import com.apass.esp.domain.Response;
+import com.apass.esp.domain.dto.monitor.MonitorDto;
+import com.apass.esp.service.monitor.MonitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by xianzhi.wang on 2017/5/18.
+ */
+@Controller
+@RequestMapping(value = "/monitor")
+public class MonitorController {
+
+    @Autowired
+    private MonitorService monitorService;
+
+
+    @RequestMapping(value = "/addMonitorLog", method = RequestMethod.POST)
+    public Response addMonitorLog(@RequestBody MonitorDto monitorDto) {
+
+        int record = monitorService.insertMonitor(monitorDto);
+        return Response.success("添加成功", record);
+    }
+
+}
