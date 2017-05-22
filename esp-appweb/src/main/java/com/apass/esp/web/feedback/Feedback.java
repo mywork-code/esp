ee14472ef5602e57e9c40bcb0276c416a28d3cfa
unknown <zengqingshan@vcredit.cn>
2017-05-22 14:26:24 +0800

    app端意见反馈接口

---
+++ esp-appweb/src/main/java/com/apass/esp/web/feedback/Feedback.java
@@ -0,0 +1,57 @@
+package com.apass.esp.web.feedback;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.apass.esp.domain.Response;
+import com.apass.esp.domain.entity.FeedBack;
+import com.apass.esp.mapper.FeedBackMapper;
+import com.apass.esp.web.activity.ActivityWithDrawController;
+import com.apass.gfb.framework.utils.CommonUtils;
+
+@Controller
+@RequestMapping("v1/feedback")
+public class Feedback {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ActivityWithDrawController.class);
+
+	@Autowired
+	public FeedBackMapper feedBackMapper;
+	
+	/**
+	 * 意见反馈保存
+	 */
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@ResponseBody
+	public Response saveFeedback(@RequestBody Map<String, Object> paramMap) {
+		String feedbackType = CommonUtils.getValue(paramMap, "feedbackType");//意见反馈类型
+		String comments = CommonUtils.getValue(paramMap, "comments");//意见反馈内容
+		String mobile = CommonUtils.getValue(paramMap, "mobile");//反馈者手机号
+
+		if (StringUtils.isAnyBlank(feedbackType,comments,mobile)) {
+			return Response.fail("意见反馈失败！");
+		}
+		Date date=new Date();
+//		SimpleDateFormat sdft=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		String createDate =sdft.format(date);
+		FeedBack fb=new FeedBack();
+		fb.setFeedbackType(feedbackType);
+		fb.setComments(comments);
+		fb.setMobile(mobile);
+		fb.setCreateDate(date);
+		Integer result=feedBackMapper.insert(fb);
+		if(result==1){
+			return Response.success("意见反馈保存成功!");
+		}
+		return Response.fail("意见反馈失败！");
+	}
+}
