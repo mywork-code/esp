2e16e5e5edd9bf424a4c0321ba8fec292987e85f
pengyingchao <pengyingchao@apass.cn>
2017-05-23 15:00:11 +0800

    去掉alert

--- esp-ams/src/main/resources/static/WebContent/js/goods/goodCategory.js
+++ esp-ams/src/main/resources/static/WebContent/js/goods/goodCategory.js
@@ -62,7 +62,6 @@ $(function() {
 			align : 'center',
 			sortable : true ,
 			formatter : function(value, row, index) {
-				alert(index);
                 var content = "";
                 // 编辑
                 content += "<a href='javascript:void(0);' class='easyui-linkedbutton'";
