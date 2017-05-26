4dd3d4d1a6b5adb15484b1a0c1145a9c7fb66367
sunchaohai <sunchaohai@apass.com>
2017-05-26 13:57:53 +0800

    加密方法去掉注释@Around

--- esp-appweb/src/main/java/com/apass/esp/inteceptor/EspAppRSAHandler.java
+++ esp-appweb/src/main/java/com/apass/esp/inteceptor/EspAppRSAHandler.java
@@ -6,9 +6,11 @@ import com.apass.gfb.framework.utils.BaseConstants.CommonCode;
 import com.apass.gfb.framework.utils.GsonUtils;
 import com.apass.gfb.framework.utils.RSAUtils;
 import com.google.common.collect.Maps;
+
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
@@ -50,7 +52,7 @@ public class EspAppRSAHandler {
 	 * @return Object
 	 * @throws Throwable
 	 */
-//	@Around("execution(* com.apass.esp.web..*.*(..)) || execution(* com.apass.esp.noauth..*.*(..))")
+	@Around("execution(* com.apass.esp.web..*.*(..)) || execution(* com.apass.esp.noauth..*.*(..))")
 	private Object handleRSAInteceptor(ProceedingJoinPoint point) throws Throwable {
 		Object[] arr = point.getArgs();
 		if (arr == null || arr.length == 0) {
