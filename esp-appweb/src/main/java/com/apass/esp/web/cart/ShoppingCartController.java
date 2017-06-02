e3260ecad2e688ad6559f4a189f329ed1929477e
xujie <xujie@apass.cn>
2017-06-02 16:20:49 +0800

    errorcode

--- esp-appweb/src/main/java/com/apass/esp/web/cart/ShoppingCartController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/cart/ShoppingCartController.java
@@ -1,20 +1,5 @@
 package com.apass.esp.web.cart;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.apass.esp.common.code.BusinessErrorCode;
 import com.apass.esp.domain.Response;
 import com.apass.esp.domain.dto.cart.GoodsIsSelectDto;
@@ -28,6 +13,19 @@ import com.apass.gfb.framework.logstash.LOG;
 import com.apass.gfb.framework.utils.BaseConstants.ParamsCode;
 import com.apass.gfb.framework.utils.CommonUtils;
 import com.apass.gfb.framework.utils.GsonUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Path("/cart")
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -76,10 +74,10 @@ public class ShoppingCartController {
 
         } catch (BusinessException e) {
             LOG.logstashException(requestId, methodDesc, e.getErrorDesc(), e);
-            return Response.fail(BusinessErrorCode.ADD_INFO_FAILED);
+            return Response.fail(BusinessErrorCode.GOODS_ADDTOCART_ERROR);
         } catch (Exception e) {
             LOG.logstashException(requestId, methodDesc, e.getMessage(), e);
-            return Response.fail(BusinessErrorCode.ADD_INFO_FAILED);
+            return Response.fail(BusinessErrorCode.GOODS_ADDTOCART_ERROR);
         }
     }
 
