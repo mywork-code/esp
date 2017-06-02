b06452682e21c51ae11b8df848bef8799550ff58
xujie <xujie@apass.cn>
2017-06-02 14:53:01 +0800

    default businessErrorCode

--- esp-framework/src/main/java/com/apass/gfb/framework/exception/BusinessException.java
+++ esp-framework/src/main/java/com/apass/gfb/framework/exception/BusinessException.java
@@ -17,7 +17,7 @@ public class BusinessException extends Exception {
     private BusinessErrorCode businessErrorCode;
 
     public BusinessErrorCode getBusinessErrorCode() {
-        return businessErrorCode;
+        return businessErrorCode == null ? BusinessErrorCode.NO : businessErrorCode;
     }
 
     public void setBusinessErrorCode(BusinessErrorCode businessErrorCode) {
