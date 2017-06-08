d30c1848561b92991aa2e89aeb77279a986a1a0a
wangxianzhi <wangxianzhi@apass.com>
2017-06-08 16:45:46 +0800

    test

--- esp-appweb/src/main/java/com/apass/esp/web/thirdparty/jd/TestController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/thirdparty/jd/TestController.java
@@ -1,10 +1,14 @@
 package com.apass.esp.web.thirdparty.jd;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.apass.esp.domain.Response;
+import com.apass.esp.third.party.jd.client.JdApiResponse;
+import com.apass.esp.third.party.jd.client.JdProductApiClient;
 import com.apass.esp.third.party.jd.client.JdTokenClient;
 import com.apass.gfb.framework.cache.CacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,7 +20,9 @@ import java.util.Map;
  * @see
  * @since JDK 1.8
  */
-@RestController("jd")
+
+@Controller
+@RequestMapping("jd")
 public class TestController {
     private static final String JD_TOKEN_REDIS_KEY = "JD_TOKEN_REDIS_KEY";
 
@@ -26,11 +32,30 @@ public class TestController {
     @Autowired
     private JdTokenClient jdTokenClient;
 
+    @Autowired
+    private JdProductApiClient jdProductApiClient;
+
     @RequestMapping(value = "/test", method = RequestMethod.POST)
     @ResponseBody
     public Response test(@RequestBody Map<String, Object> paramMap){
-        JSONObject jsonObject = jdTokenClient.getToken();
-        cacheManager.set(JD_TOKEN_REDIS_KEY, jsonObject.toJSONString());
-        return Response.success("1",jsonObject);
+       // JSONObject jsonObject = jdTokenClient.getToken();
+        //cacheManager.set(JD_TOKEN_REDIS_KEY, jsonObject.toJSONString());
+        return Response.success("1","");
     }
+
+    @RequestMapping(value = "/productPageNumQuery", method = RequestMethod.POST)
+    @ResponseBody
+    public Response productPageNumQuery(@RequestBody Map<String, Object> paramMap){
+        JdApiResponse<JSONArray> jdApiResponse =  jdProductApiClient.productPageNumQuery();
+        return Response.success("1",jdApiResponse);
+    }
+
+
+    @RequestMapping(value = "/productSkuQuery", method = RequestMethod.POST)
+    @ResponseBody
+    public Response productSkuQuery(@RequestBody Map<String, Object> paramMap){
+        JdApiResponse<String> jdApiResponse =  jdProductApiClient.productSkuQuery(100);
+        return Response.success("1",jdApiResponse);
+    }
+
 }
