e62b46017128e7c9759373cd4e6fd79053803bb4
xujie <xujie@apass.cn>
2017-06-15 10:40:57 +0800

    sql

--- esp-ams/src/main/resources/doc/sprint5_0622.sql
+++ esp-ams/src/main/resources/doc/sprint5_0622.sql
@@ -7,3 +7,6 @@ ALTER TABLE esp.`t_esp_award_bind_rel`
 ADD COLUMN `name`  varchar(20)  COMMENT '真实姓名';
 ALTER TABLE esp.`t_esp_award_bind_rel`
 ADD COLUMN `invite_name`  varchar(20)  COMMENT '邀请人真实姓名';
+
+ALTER TABLE esp.`t_esp_goods_base_info`
+ADD COLUMN `un_support_province`  varchar(255) NULL COMMENT '不配送区域';
