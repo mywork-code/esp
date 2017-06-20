3324d58c8ceee3afe72fd48c336947559efcb416
sunchaohai <sunchaohai@apass.com>
2017-06-20 16:38:10 +0800

    conflict solve

--- esp-service/src/main/java/com/apass/esp/mapper/TxnInfoMapper.java
+++ esp-service/src/main/java/com/apass/esp/mapper/TxnInfoMapper.java
@@ -1,6 +1,7 @@
 package com.apass.esp.mapper;
 
 import com.apass.esp.domain.entity.bill.TxnInfoEntity;
+
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
@@ -16,8 +17,6 @@ public interface TxnInfoMapper {
    */
   List<TxnInfoEntity> selectByOrderId(String orderId);
 
-<<<<<<< HEAD
-  
    /**
      * 根据orderId查询去交易流水表的OrigOryid(原始消费交易的queryId)
 	 * @param orderId:订单id
@@ -25,8 +24,7 @@ public interface TxnInfoMapper {
 	 */
   String queryOrigTxnIdByOrderid(String orderId);
 
-=======
   void updateTime(@Param("orderId") String orderId,@Param("date") Date date);
->>>>>>> 6252aaa2c274a09011e94f8e2d93c1a40f5b5b5b
+
 
 }
