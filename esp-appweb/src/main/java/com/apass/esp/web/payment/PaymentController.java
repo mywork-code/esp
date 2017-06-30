107009e88fc383975d5c178f15109e953578b059
unknown <zengqingshan@vcredit.cn>
2017-06-30 18:14:23 +0800

    支付方式选择确认（价格变动）

--- esp-appweb/src/main/java/com/apass/esp/web/payment/PaymentController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/payment/PaymentController.java
@@ -141,8 +141,14 @@ public class PaymentController {
             PayInfoEntity payInfo = paymentService.confirmPayMethod(requestId , userId, orderList,paymentType,downPayType);
             resultMap.put("resultMap", payInfo);
         } catch (BusinessException e) {
-            LOGGER.error(e.getErrorDesc(), e);
-            return Response.fail(e.getErrorDesc(),e.getBusinessErrorCode());
+        	if(BusinessErrorCode.GOODS_PRICE_CHANGE_ERROR.equals(e.getBusinessErrorCode())){//商品价格已变动，请重新下单
+    			orderDao.updateStatusByOrderId(e.getErrorCode(), OrderStatus.ORDER_CANCEL.getCode());
+    			LOGGER.error(e.getErrorDesc(), e);
+    			return Response.fail("商品价格已变动，请重新下单");
+        	}else{
+    		   LOGGER.error(e.getErrorDesc(), e);
+               return Response.fail(e.getErrorDesc(),e.getBusinessErrorCode());
+        	}
         } catch (Exception e) {
             LOGGER.error("订单支付失败", e);
             return Response.fail(BusinessErrorCode.ORDER_PAY_FAILED);
