e3e9fafe7c1ef0d31e8557e612d2a13fe3a27506
wangxianzhi <wangxianzhi@apass.com>
2017-07-04 15:39:08 +0800

    mapper

---
+++ esp-service/src/main/java/com/apass/esp/mapper/JdGategoryMapper.java
@@ -0,0 +1,8 @@
+package com.apass.esp.mapper;
+
+import com.apass.esp.third.party.jd.entity.base.JdGategory;
+import com.apass.gfb.framework.mybatis.GenericMapper;
+
+public interface JdGategoryMapper extends GenericMapper<JdGategory,Long> {
+
+}
