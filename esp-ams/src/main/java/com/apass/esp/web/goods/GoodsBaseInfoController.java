c6cac8256724c4f9a8bc55e71dc5eb3543481b1e
unknown <zengqingshan@vcredit.cn>
2017-07-10 14:42:27 +0800

    后台预览京东修改

--- esp-ams/src/main/java/com/apass/esp/web/goods/GoodsBaseInfoController.java
+++ esp-ams/src/main/java/com/apass/esp/web/goods/GoodsBaseInfoController.java
@@ -835,15 +835,11 @@ public class GoodsBaseInfoController {
         Map<String, Object> map = Maps.newHashMap();
         String id = HttpWebUtils.getValue(request, "id");
         String view = HttpWebUtils.getValue(request, "view");
-        String skuId = HttpWebUtils.getValue(request, "skuId");
-        if(StringUtils.isNotEmpty(skuId)){
-            map = jdGoodsInfoService.getJdGoodsAllInfoBySku(Long.valueOf(skuId));
-            return new ModelAndView("goods/goodsPreviewProductJD-view", map);
-        }
+ 
         GoodsInfoEntity goodsInfo = goodsService.selectByGoodsId(Long.valueOf(id));
         String externalId = goodsInfo.getExternalId();// 外部商品id
-        externalId = "3245084";
         map = jdGoodsInfoService.getJdGoodsAllInfoBySku(Long.valueOf(externalId).longValue());
+        map.put("view", view);
         return new ModelAndView("goods/goodsPreviewProductJD-view", map);
     }
 
