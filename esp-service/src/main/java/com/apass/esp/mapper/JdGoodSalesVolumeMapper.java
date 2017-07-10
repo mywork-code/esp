fce6805c8338f7bc072a159d15effe960f9172b1
wangxianzhi <wangxianzhi@apass.com>
2017-07-10 18:26:42 +0800

    mapper

---
+++ esp-service/src/main/java/com/apass/esp/mapper/JdGoodSalesVolumeMapper.java
@@ -0,0 +1,10 @@
+package com.apass.esp.mapper;
+
+
+import com.apass.esp.domain.entity.JdGoodSalesVolume;
+import com.apass.gfb.framework.mybatis.GenericMapper;
+
+public interface JdGoodSalesVolumeMapper extends GenericMapper<JdGoodSalesVolume, Long> {
+
+
+}
