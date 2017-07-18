fb0fd8be832b38d29cc69118176c693cb77ce538
unknown <zengqingshan@vcredit.cn>
2017-07-18 15:06:51 +0800

    获取商品详细信息 基本信息+详细信息（对接京东接口）

--- esp-appweb/src/main/java/com/apass/esp/noauth/home/ShopHomeController.java
+++ esp-appweb/src/main/java/com/apass/esp/noauth/home/ShopHomeController.java
@@ -390,10 +390,6 @@ public class ShopHomeController {
             	LOGGER.error("商品号不能为空!");
                 return Response.fail(BusinessErrorCode.PARAM_IS_EMPTY);
             }
-            if (!StringUtils.isEmpty(userId)) {
-                int amountInCart = shoppingCartService.getNumOfTypeInCart(userId);
-                returnMap.put("amountInCart", amountInCart);
-            }
             //查看地址信息
             AddressInfoEntity  addty=new AddressInfoEntity();
             //查询京东地址
@@ -429,6 +425,11 @@ public class ShopHomeController {
             	returnMap.put("source", "jd");
             }else{
                 goodService.loadGoodsBasicInfoById(goodsId,returnMap);
+            }
+            //获取购物车中商品种类数
+            if (!StringUtils.isEmpty(userId)) {
+                int amountInCart = shoppingCartService.getNumOfTypeInCart(userId);
+                returnMap.put("amountInCart", amountInCart);
             }
             returnMap.put("address", addressInfoList);
             return Response.success("加载成功", returnMap);
