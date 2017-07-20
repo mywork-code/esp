5ac532e96c66381a1ae7efdfd66ca8ba096d7af4
wangxianzhi <wangxianzhi@apass.com>
2017-07-20 16:58:16 +0800

    update

--- esp-service/src/main/java/com/apass/esp/repository/refund/RefundDetailInfoRepository.java
+++ esp-service/src/main/java/com/apass/esp/repository/refund/RefundDetailInfoRepository.java
@@ -4,7 +4,18 @@ import com.apass.esp.domain.entity.refund.RefundDetailInfoEntity;
 import com.apass.gfb.framework.annotation.MyBatisRepository;
 import com.apass.gfb.framework.mybatis.support.BaseMybatisRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @MyBatisRepository
-public class RefundDetailInfoRepository extends BaseMybatisRepository<RefundDetailInfoEntity, Long>{
+public class RefundDetailInfoRepository extends BaseMybatisRepository<RefundDetailInfoEntity, Long> {
+
+    public int updateByStatusAndGoodsId(RefundDetailInfoEntity refundDetailInfoEntity) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("orderId", refundDetailInfoEntity.getOrderId());
+        hashMap.put("goodsId", refundDetailInfoEntity.getGoodsId());
+        hashMap.put("status", refundDetailInfoEntity.getStatus());
+        return getSqlSession().update(getSQL("updateByStatusAndGoodsId"), hashMap);
+    }
 
 }
