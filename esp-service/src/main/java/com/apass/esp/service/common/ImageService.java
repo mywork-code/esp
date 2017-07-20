428fa1b37970bf506d86ee5af4578df3c88a2a1b
unknown <zengqingshan@vcredit.cn>
2017-07-20 10:31:47 +0800

    购物车改造100

--- esp-service/src/main/java/com/apass/esp/service/common/ImageService.java
+++ esp-service/src/main/java/com/apass/esp/service/common/ImageService.java
@@ -32,7 +32,22 @@ public class ImageService {
             return espImageUri + "/static" + imageUrl;
         }
     }
-
+    /**
+     * @param imageUrl 加密前的京东图片路径
+     * 京东图片需要在前面添加http://img13.360buyimg.com/n0/
+     * 其中n0(最大图)、n1(350*350px)、n2(160*160px)、n3(130*130px)、n4(100*100px) 为图片大小
+     * @return String
+     */
+    public String getJDImageUrl(String imageUrl,String n) throws BusinessException {
+        if (StringUtils.isBlank(espImageUri)) {
+            throw new BusinessException("espImageUri为空");
+        }
+        if (StringUtils.isBlank(imageUrl)) {
+            return null;
+        } else {
+            return "http://img13.360buyimg.com/" +n+ "/" + imageUrl;
+        }
+    }
     /**
      * 加密后的
      * @param imageUrl
