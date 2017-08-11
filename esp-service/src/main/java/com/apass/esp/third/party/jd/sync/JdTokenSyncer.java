68d1d210ba1c3694a627bfbe285ffddf93d94b92
wangxianzhi <wangxianzhi@apass.com>
2017-08-11 10:23:33 +0800

    token

--- esp-service/src/main/java/com/apass/esp/third/party/jd/sync/JdTokenSyncer.java
+++ esp-service/src/main/java/com/apass/esp/third/party/jd/sync/JdTokenSyncer.java
@@ -1,8 +1,10 @@
 package com.apass.esp.third.party.jd.sync;
 
+import com.alibaba.fastjson.JSONObject;
 import com.apass.esp.third.party.jd.client.JdTokenClient;
 import com.apass.gfb.framework.cache.CacheManager;
 import com.apass.gfb.framework.environment.SystemEnvConfig;
+import com.apass.gfb.framework.utils.DateFormatUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,10 +33,16 @@ public class JdTokenSyncer extends AbstractSyncer {
 
     @Override
     public void run() {
-//        if(systemEnvConfig.isPROD()){
-//            JSONObject jsonObject = jdTokenClient.getToken();
-//            cacheManager.set(JD_TOKEN_REDIS_KEY, jsonObject.toJSONString());
-//        }
+        if (systemEnvConfig.isPROD()) {
+            String json = cacheManager.get(JD_TOKEN_REDIS_KEY);
+            JSONObject jsonObject = JSONObject.parseObject(json);
+            String time = jsonObject.getString("time");
+            long interVal = System.currentTimeMillis() - Long.valueOf(time);
+            if (3600 * 24 * 7 <= interVal) {
+                JSONObject jsonObject1 = jdTokenClient.getToken();
+                cacheManager.set(JD_TOKEN_REDIS_KEY, jsonObject1.toJSONString());
+            }
+        }
     }
 
     @Override
