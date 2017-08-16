1c5d006ed3853f83a9e3edf6705288cc29557ce9
xujie <xujie@apass.cn>
2017-08-16 15:04:55 +0800

    sql

--- esp-ams/src/main/resources/doc/sprint8.sql
+++ esp-ams/src/main/resources/doc/sprint8.sql
@@ -3,7 +3,7 @@ CREATE TABLE `t_esp_search_keys` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `KEY_TYPE` varchar(1) NOT NULL DEFAULT '0' COMMENT '类型(0:最近搜索 1:热门搜索)',
   `KEY_VALUE` varchar(200) NOT NULL DEFAULT '' COMMENT '内容',
-  `USER_ID` varchar(200) NOT NULL DEFAULT '' COMMENT '用户ID',
+  `USER_ID` varchar(32) NOT NULL DEFAULT '' COMMENT '用户ID',
  `KEY_STATUS` varchar(1) NOT NULL DEFAULT '0' COMMENT '类型(0:可用 1:删除)',
   `CREATE_DATE` datetime NOT NULL COMMENT '创建时间',
   `UPDATE_DATE` datetime NOT NULL COMMENT '更新时间',
