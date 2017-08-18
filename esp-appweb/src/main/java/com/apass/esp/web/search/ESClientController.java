877f01bbc945c28482312ef08d154edcfa417b47
wangxianzhi <wangxianzhi@apass.com>
2017-08-18 15:46:37 +0800

    update

--- esp-appweb/src/main/java/com/apass/esp/web/search/ESClientController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/search/ESClientController.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.apass.esp.search.utils.HanyuPinyinHelper;
 import org.apache.commons.lang3.StringUtils;
-import org.elasticsearch.client.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,9 +33,7 @@ import com.apass.esp.search.entity.Goods;
 import com.apass.esp.search.entity.GoodsVo;
 import com.apass.esp.search.enums.IndexType;
 import com.apass.esp.search.enums.SortMode;
-import com.apass.esp.search.manager.ESClientManager;
 import com.apass.esp.search.manager.IndexManager;
-import com.apass.esp.search.utils.Pinyin4jUtil;
 import com.apass.esp.service.common.ImageService;
 import com.apass.esp.service.goods.GoodsService;
 import com.apass.esp.service.jd.JdGoodsInfoService;
@@ -79,7 +77,7 @@ public class ESClientController {
     @RequestMapping(value = "addData", method = RequestMethod.POST)
     @ResponseBody
     public Response addData(@RequestBody Map<String, Object> paramMap) {
-        List<Goods> goodsList = new ArrayList<>();
+        List<Goods> goodsList = new ArrayList<>(15000);
         List<GoodsInfoEntity> selectByCategoryId2 = goodsService.selectUpGoods();
         for (GoodsInfoEntity g : selectByCategoryId2) {
         	Goods goods = GoodsInfoToGoods(g);
@@ -106,24 +104,24 @@ public class ESClientController {
     	}
     	goods.setCategoryName1(cate1.getCategoryName());
     	goods.setCategoryId1(g.getCategoryId1());
-    	goods.setCategoryName1Pinyin(Pinyin4jUtil.converterToSpell(cate1.getCategoryName()));
+    	goods.setCategoryName1Pinyin(HanyuPinyinHelper.getPinyinString(cate1.getCategoryName()));
 
     	Category cate2 = categoryMapper.selectByPrimaryKey(g.getCategoryId2());
     	if(null == cate2){
     		return null;
     	}
     	goods.setCategoryName2(cate2.getCategoryName());
-    	goods.setCategoryName2Pinyin(Pinyin4jUtil.converterToSpell(cate2.getCategoryName()));
+    	goods.setCategoryName2Pinyin(HanyuPinyinHelper.getPinyinString(cate2.getCategoryName()));
     	goods.setCategoryId3(g.getCategoryId3());
     	Category cate3 = categoryMapper.selectByPrimaryKey(g.getCategoryId3());
     	if(null == cate3){
     		return null;
     	}
     	goods.setCategoryName3(cate3.getCategoryName());
-    	goods.setCategoryName3Pinyin(Pinyin4jUtil.converterToSpell(cate3.getCategoryName()));
+    	goods.setCategoryName3Pinyin(HanyuPinyinHelper.getPinyinString(cate3.getCategoryName()));
 
     	goods.setGoodsName(g.getGoodsName());
-    	goods.setGoodsNamePinyin(Pinyin4jUtil.converterToSpell(g.getGoodsName()));
+    	goods.setGoodsNamePinyin(HanyuPinyinHelper.getPinyinString(g.getGoodsName()));
     	goods.setDelistTime(g.getDelistTime());
     	goods.setGoodsLogoUrl(g.getGoodsLogoUrl());
     	if(StringUtils.equals(g.getSource(), SourceType.JD.getCode())){
@@ -140,7 +138,7 @@ public class ESClientController {
     	goods.setDelistTimeString(DateFormatUtil.dateToString(g.getDelistTime(),""));
     	goods.setCreateDate(g.getCreateDate());
     	goods.setGoodsTitle(g.getGoodsTitle());
-    	goods.setGoodsTitlePinyin(Pinyin4jUtil.converterToSpell(g.getGoodsTitle()));
+    	goods.setGoodsTitlePinyin(HanyuPinyinHelper.getPinyinString(g.getGoodsTitle()));
     	goods.setListTime(g.getListTime());
     	goods.setListTimeString(DateFormatUtil.dateToString(g.getListTime(),""));
     	goods.setNewCreatDate(g.getNewCreatDate());
@@ -183,7 +181,7 @@ public class ESClientController {
         	}else{
         		goods.setGoodsSkuAttr(String.valueOf(params.get("minSkuAttr")));
         	}
-        	goods.setGoodsSkuAttrPinyin(Pinyin4jUtil.converterToSpell(goods.getGoodsSkuAttr()));
+        	goods.setGoodsSkuAttrPinyin(HanyuPinyinHelper.getPinyinString(goods.getGoodsSkuAttr()));
 		} catch (Exception e) {
 			 e.getStackTrace();
 			 return null;
