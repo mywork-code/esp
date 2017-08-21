a4f9a7a97e7a31d98451b8ed12a1cdf9fa33b7db
pengyingchao <pengyingchao@apass.cn>
2017-08-21 18:31:53 +0800

    add Data

--- esp-appweb/src/main/java/com/apass/esp/web/search/ESClientController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/search/ESClientController.java
@@ -1,9 +1,6 @@
 package com.apass.esp.web.search;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,27 +18,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.apass.esp.common.code.BusinessErrorCode;
 import com.apass.esp.common.utils.JsonUtil;
 import com.apass.esp.domain.Response;
-import com.apass.esp.domain.entity.Category;
-import com.apass.esp.domain.entity.JdGoodSalesVolume;
 import com.apass.esp.domain.entity.goods.GoodsInfoEntity;
 import com.apass.esp.domain.enums.CategorySort;
-import com.apass.esp.domain.enums.SourceType;
-import com.apass.esp.mapper.CategoryMapper;
-import com.apass.esp.mapper.JdGoodSalesVolumeMapper;
 import com.apass.esp.search.condition.GoodsSearchCondition;
 import com.apass.esp.search.entity.Goods;
 import com.apass.esp.search.entity.GoodsVo;
 import com.apass.esp.search.enums.IndexType;
 import com.apass.esp.search.enums.SortMode;
 import com.apass.esp.search.manager.IndexManager;
-import com.apass.esp.search.utils.HanyuPinyinHelper;
-import com.apass.esp.service.common.ImageService;
 import com.apass.esp.service.goods.GoodsService;
-import com.apass.esp.service.jd.JdGoodsInfoService;
-import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.mybatis.page.Pagination;
 import com.apass.gfb.framework.utils.CommonUtils;
-import com.apass.gfb.framework.utils.DateFormatUtil;
 
 /**
  * Created by xianzhi.wang on 2017/5/16.
@@ -55,15 +42,6 @@ public class ESClientController {
     
     @Autowired
     private GoodsService goodsService;
-    @Autowired
-    private CategoryMapper categoryMapper;
-    @Autowired
-    private ImageService imageService;
-    @Autowired
-    private JdGoodSalesVolumeMapper jdGoodSalesVolumeMapper;
-    @Autowired
-    private JdGoodsInfoService jdGoodsInfoService;
-    
     
     @RequestMapping(value = "addData", method = RequestMethod.POST)
     @ResponseBody
@@ -87,103 +65,6 @@ public class ESClientController {
         return Response.successResponse();
     }
 
-
-    public Goods GoodsInfoToGoods(GoodsInfoEntity g){
-    	Goods goods = new Goods();
-    	goods.setId(Integer.valueOf(g.getId()+""));
-    	goods.setGoodId(g.getGoodId());
-    	goods.setCategoryId1(g.getCategoryId1());
-    	Category cate1 = categoryMapper.selectByPrimaryKey(g.getCategoryId1());
-    	if(null == cate1){
-    		return null;
-    	}
-    	goods.setCategoryName1(cate1.getCategoryName());
-    	goods.setCategoryId1(g.getCategoryId1());
-    	goods.setCategoryName1Pinyin(HanyuPinyinHelper.getPinyinString(cate1.getCategoryName()));
-
-    	Category cate2 = categoryMapper.selectByPrimaryKey(g.getCategoryId2());
-    	if(null == cate2){
-    		return null;
-    	}
-    	goods.setCategoryName2(cate2.getCategoryName());
-    	goods.setCategoryName2Pinyin(HanyuPinyinHelper.getPinyinString(cate2.getCategoryName()));
-    	goods.setCategoryId3(g.getCategoryId3());
-    	Category cate3 = categoryMapper.selectByPrimaryKey(g.getCategoryId3());
-    	if(null == cate3){
-    		return null;
-    	}
-    	goods.setCategoryName3(cate3.getCategoryName());
-    	goods.setCategoryName3Pinyin(HanyuPinyinHelper.getPinyinString(cate3.getCategoryName()));
-
-    	goods.setGoodsName(g.getGoodsName());
-    	goods.setGoodsNamePinyin(HanyuPinyinHelper.getPinyinString(g.getGoodsName()));
-    	goods.setDelistTime(g.getDelistTime());
-    	goods.setGoodsLogoUrl(g.getGoodsLogoUrl());
-    	if(StringUtils.equals(g.getSource(), SourceType.JD.getCode())){
-    		goods.setGoodsLogoUrlNew("http://img13.360buyimg.com/n3/" + g.getGoodsLogoUrl());
-    	}else{
-    		try {
-				goods.setGoodsLogoUrlNew(imageService.getImageUrl(g.getGoodsLogoUrl()));
-			} catch (BusinessException e) {
-				e.getStackTrace();
-			}
-    	}
-
-    	goods.setSource(g.getSource());
-    	goods.setDelistTimeString(DateFormatUtil.dateToString(g.getDelistTime(),""));
-    	goods.setCreateDate(g.getCreateDate());
-    	goods.setGoodsTitle(g.getGoodsTitle());
-    	goods.setGoodsTitlePinyin(HanyuPinyinHelper.getPinyinString(g.getGoodsTitle()));
-    	goods.setListTime(g.getListTime());
-    	goods.setListTimeString(DateFormatUtil.dateToString(g.getListTime(),""));
-    	goods.setNewCreatDate(g.getNewCreatDate());
-    	goods.setUpdateDate(g.getUpdateDate());
-
-    	List<JdGoodSalesVolume> getJdGoodSalesVolume =
-    			jdGoodSalesVolumeMapper.getJdGoodSalesVolumeByGoodsId(g.getGoodId());
-
-    	int goodsSum = 0;
-    	int goodsSum30 = 0;
-    	Date date = new Date();
-    	for (JdGoodSalesVolume jd : getJdGoodSalesVolume) {
-    		goodsSum += jd.getSalesNum();
-    		if(jd.getCreateDate().before(date) && jd.getCreateDate().after(DateFormatUtil.addDays(date, -30))){
-    			goodsSum30 += jd.getSalesNum();
-    		}
-		}
-    	goods.setSaleNum(goodsSum);
-    	goods.setSaleNumFor30(goodsSum30);
-    	try {
-    		Map<String, Object> params = goodsService.getMinPriceGoods(g.getGoodId());
-    		if(params.isEmpty()){
-        		return null;
-        	}
-    		goods.setGoodsPrice(new BigDecimal(String.valueOf(params.get("minPrice"))));
-        	goods.setFirstPrice(new BigDecimal(String.valueOf(params.get("minPrice"))).multiply(new BigDecimal(0.1)) .setScale(2, BigDecimal.ROUND_CEILING));
-        	goods.setGoodsStockId(Long.valueOf(String.valueOf(params.get("minPriceStockId"))));
-        	if(StringUtils.equals(goods.getSource(),SourceType.JD.getCode())){
-        		Map<String, Object> descMap = new HashMap<String, Object>();
-                try {
-                    descMap = jdGoodsInfoService.getJdGoodsSimilarSku(Long.valueOf(g.getExternalId()));
-                } catch (Exception e) {
-                }
-                String jdGoodsSimilarSku = (String) descMap.get("jdGoodsSimilarSku");
-                int jdSimilarSkuListSize = (int) descMap.get("jdSimilarSkuListSize");
-                if (StringUtils.isNotBlank(jdGoodsSimilarSku) && jdSimilarSkuListSize > 0) {
-                	goods.setGoodsSkuAttr(jdGoodsSimilarSku);
-                }
-
-        	}else{
-        		goods.setGoodsSkuAttr(String.valueOf(params.get("minSkuAttr")));
-        	}
-        	goods.setGoodsSkuAttrPinyin(HanyuPinyinHelper.getPinyinString(goods.getGoodsSkuAttr()));
-		} catch (Exception e) {
-			 e.getStackTrace();
-			 return null;
-		}
-    	return goods;
-    }
-    
     /**
      * 查询
      *
