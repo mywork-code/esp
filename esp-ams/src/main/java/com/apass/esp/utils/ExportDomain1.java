d6fe32a40882c33a331b7379245fa14752d9c794
wangxianzhi <wangxianzhi@apass.com>
2017-09-07 12:31:25 +0800

    update

--- esp-ams/src/main/java/com/apass/esp/utils/ExportDomain1.java
+++ esp-ams/src/main/java/com/apass/esp/utils/ExportDomain1.java
@@ -1,5 +1,7 @@
 package com.apass.esp.utils;
 
+import java.math.BigDecimal;
+
 /**
  * type: class
  *
@@ -9,31 +11,67 @@ package com.apass.esp.utils;
  * @since JDK 1.8
  */
 public class ExportDomain1 {
-    private String date;
-    private String amt ;
-    private String price;
+    private String type;
+    private String d02;
+    private String d03;
+    private String d04;
+    private String d05;
+    private String d09;
+    private String d10;
 
-    public String getDate() {
-        return date;
+    public String getType() {
+        return type;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getAmt() {
-        return amt;
+    public String getD02() {
+        return d02;
     }
 
-    public void setAmt(String amt) {
-        this.amt = amt;
+    public void setD02(String d02) {
+        this.d02 = d02;
     }
 
-    public String getPrice() {
-        return price;
+    public String getD03() {
+        return d03;
     }
 
-    public void setPrice(String price) {
-        this.price = price;
+    public void setD03(String d03) {
+        this.d03 = d03;
+    }
+
+    public String getD04() {
+        return d04;
+    }
+
+    public void setD04(String d04) {
+        this.d04 = d04;
+    }
+
+    public String getD05() {
+        return d05;
+    }
+
+    public void setD05(String d05) {
+        this.d05 = d05;
+    }
+
+    public String getD09() {
+        return d09;
+    }
+
+    public void setD09(String d09) {
+        this.d09 = d09;
+    }
+
+    public String getD10() {
+        return d10;
+    }
+
+    public void setD10(String d10) {
+        this.d10 = d10;
     }
 }
