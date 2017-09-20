aa6e43709016bf17f4a3cfb04f771f68c26de5b6
unknown <zengqingshan@vcredit.cn>
2017-09-20 18:01:05 +0800

    推送消息监听

---
+++ esp-domain/src/main/java/com/apass/esp/domain/entity/MessageListener.java
@@ -0,0 +1,95 @@
+package com.apass.esp.domain.entity;
+
+import java.util.Date;
+
+public class MessageListener {
+    private Long id;
+
+    private String type;
+
+    private String skuid;
+
+    private String orderid;
+
+    private String status;
+
+    private String errorMassage;
+
+    private String result;
+
+    private Date createDate;
+
+    private Date updateDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSkuid() {
+        return skuid;
+    }
+
+    public void setSkuid(String skuid) {
+        this.skuid = skuid;
+    }
+
+    public String getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(String orderid) {
+        this.orderid = orderid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getErrorMassage() {
+        return errorMassage;
+    }
+
+    public void setErrorMassage(String errorMassage) {
+        this.errorMassage = errorMassage;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
