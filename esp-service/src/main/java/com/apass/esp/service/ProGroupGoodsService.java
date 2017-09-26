8fba6469ee3582985e30d1486bfd54a1848a531d
unknown <zengqingshan@vcredit.cn>
2017-09-26 17:33:55 +0800

    活动商品导入修改

--- esp-service/src/main/java/com/apass/esp/service/ProGroupGoodsService.java
+++ esp-service/src/main/java/com/apass/esp/service/ProGroupGoodsService.java
@@ -1,7 +1,11 @@
 package com.apass.esp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.apass.esp.domain.entity.ProGroupGoods;
+import com.apass.esp.mapper.ProGroupGoodsMapper;
 
 /**
  * Created by jie.xu on 17/9/26.
@@ -9,4 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(rollbackFor = { Exception.class })
 public class ProGroupGoodsService {
+	@Autowired
+	private ProGroupGoodsMapper proGroupGoodsMapper;
+	
+	public ProGroupGoods selectByGoodsId(Long goodsId){
+		return proGroupGoodsMapper.selectByGoodsId(goodsId);
+	}
+	
+	public Integer insertSelective(ProGroupGoods proGroupGoods){
+		return proGroupGoodsMapper.insertSelective(proGroupGoods);
+	}
+	
 }
