e704868fa3d30c94d3eeeb4d51a7d88abf828484
unknown <zengqingshan@vcredit.cn>
2017-09-29 10:34:08 +0800

    验证京东商品是否支持7天退货

--- esp-appweb/src/main/java/com/apass/esp/noauth/home/ShopHomeController.java
+++ esp-appweb/src/main/java/com/apass/esp/noauth/home/ShopHomeController.java
@@ -797,11 +797,15 @@ public class ShopHomeController {
                 }
                 returnMap = jdGoodsInfoService.getAppJdGoodsAllInfoBySku(
                         Long.valueOf(externalId).longValue(), goodsId.toString(),region3);
-
                 returnMap.put("goodsName", goodsInfo.getGoodsName());// 商品名称
                 returnMap.put("merchantCode", goodsInfo.getMerchantCode());// 商户编码
-                returnMap.put("support7dRefund", goodsInfo.getSupport7dRefund());// 是否支持7天无理由退货,Y、N
                 returnMap.put("activityCfg", goodsService.getActivityInfo(goodsId));// 满减活动字段
+                //验证京东商品是否支持7天退货
+                if(orderService.checkGoodsIs7ToReturn(skuNumList)){
+                    returnMap.put("support7dRefund","Y");// 是否支持7天无理由退货,Y、N
+                }else{
+                	returnMap.put("support7dRefund","N");
+                }
                 List<GoodsStockInfoEntity> jdGoodsStockInfoList = goodsStockInfoRepository
                         .loadByGoodsId(goodsId);
                 if (jdGoodsStockInfoList.size() == 1) {
