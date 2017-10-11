0bc41deba13e5dd9168bbfab661b1af128d03e44
pengyingchao <pengyingchao@apass.cn>
2017-10-11 09:47:22 +0800

    groupAndGoods

--- esp-appweb/src/main/java/com/apass/esp/web/offer/GroupGoodsController.java
+++ esp-appweb/src/main/java/com/apass/esp/web/offer/GroupGoodsController.java
@@ -3,15 +3,17 @@ package com.apass.esp.web.offer;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.apass.esp.domain.Response;
 import com.apass.esp.domain.vo.GroupManagerVo;
@@ -19,8 +21,9 @@ import com.apass.esp.service.offer.GroupManagerService;
 import com.apass.gfb.framework.utils.CommonUtils;
 import com.google.common.collect.Maps;
 
-@Controller
-@RequestMapping("activity/group")
+@Path("/activity/group")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class GroupGoodsController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(GroupGoodsController.class);
@@ -28,9 +31,8 @@ public class GroupGoodsController {
 	@Autowired
 	private GroupManagerService groupManagerService;
 	
-	
-	@RequestMapping(value = "/getGroupAndGoods", method = RequestMethod.POST)
-	@ResponseBody
+	@POST
+    @Path("/getGroupAndGoods")
 	public Response getGroupAndGoodsByGroupId(@RequestBody Map<String, Object> paramMap){
 		String activityId = CommonUtils.getValue(paramMap, "activityId");
 		if(StringUtils.isBlank(activityId)){
