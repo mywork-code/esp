746f20afcf35849fe7ba9b733b9f9dc6cb7655fd
wangxianzhi <wangxianzhi@apass.com>
2017-10-18 09:56:17 +0800

    vbs BusinessNumCvs  Test

--- esp-ams/src/main/java/com/apass/esp/schedule/SAPScheduleTask.java
+++ esp-ams/src/main/java/com/apass/esp/schedule/SAPScheduleTask.java
@@ -1,14 +1,11 @@
 package com.apass.esp.schedule;
-
 import com.apass.esp.sap.SAPService;
 import com.apass.gfb.framework.utils.DateFormatUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Calendar;
-
 /**
  * Created by jie.xu on 17/10/17.
  */
@@ -16,39 +13,43 @@ import java.util.Calendar;
 @RequestMapping("/sap/test")
 public class SAPScheduleTask {
 
-  @Value("${ftp.ip}")
-  private String ip;
+	@Value("${ftp.ip}")
+	private String ip;
+	
+	@Value("${ftp.port}")
+	private int port;
 
-  @Value("${ftp.port}")
-  private int port;
+	@Value("${ftp.username}")
+	private String username;
 
-  @Value("${ftp.username}")
-  private String username;
+	@Value("${ftp.password}")
+	private String password;
 
-  @Value("${ftp.password}")
-  private String password;
+	@Value("${ftp.base.path}")
+	private String basePath;
 
-  @Value("${ftp.base.path}")
-  private String basePath;
+	@Autowired
+	private SAPService sapService;
 
-  @Autowired
-  private SAPService sapService;
-
-  @RequestMapping("/test1")
-  public void exec(){
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.DATE,-1);
-    String caiWuPingZhengPath = basePath + "/" + DateFormatUtil.dateToString(cal.getTime(),"yyyy/MM/dd") + "/";
-    sapService.sendCaiWuPingZhengCsv(ip,port,username,password,caiWuPingZhengPath);
-    sapService.transVBSBusinessNumCvs(ip, port, username, password, caiWuPingZhengPath);
-  }
-
-  @RequestMapping("/test2")
-  public void exec2(){
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.DATE,-1);
-    String caiWuPingZhengPath = basePath + "/" + DateFormatUtil.dateToString(cal.getTime(),"yyyy/MM/dd") + "/";
-    sapService.sendPaymentOrFullPaymentCsv(ip,port,username,password,caiWuPingZhengPath);
-    sapService.transVBSBusinessNumCvs(ip, port, username, password, caiWuPingZhengPath);
-  }
+	@RequestMapping("/test1")
+	public void exec(){
+	    Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-1);
+	    String caiWuPingZhengPath = basePath + "/" + DateFormatUtil.dateToString(cal.getTime(),"yyyy/MM/dd") + "/";
+	    sapService.sendCaiWuPingZhengCsv(ip,port,username,password,caiWuPingZhengPath);
+	}
+	@RequestMapping("/test2")
+	public void exec2(){
+	    Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-1);
+	    String caiWuPingZhengPath = basePath + "/" + DateFormatUtil.dateToString(cal.getTime(),"yyyy/MM/dd") + "/";
+	    sapService.sendPaymentOrFullPaymentCsv(ip,port,username,password,caiWuPingZhengPath);
+	}
+	@RequestMapping("/testVBS")
+	public void execVBS(){
+	    Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-1);
+	    String path = basePath + "/" + DateFormatUtil.dateToString(cal.getTime(),"yyyy/MM/dd") + "/";
+	    sapService.transVBSBusinessNumCvs(ip, port, username, password, path);
+	}
 }
