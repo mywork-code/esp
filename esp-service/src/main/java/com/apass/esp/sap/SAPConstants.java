b8dabe9527c4bce418e3b06c65f655b165a65721
haotian <754103566@qq.com>
2017-10-18 10:15:16 +0800

    name tag  删除字符

--- esp-service/src/main/java/com/apass/esp/sap/SAPConstants.java
+++ esp-service/src/main/java/com/apass/esp/sap/SAPConstants.java
@@ -10,14 +10,12 @@ public final class SAPConstants {
 
 	//平台编码
 	public static final String PLATFORM_CODE = "PT0004";//中原项目组（安家趣花）
-
 	public static final String CAIWUPINGZHENG_FILE_PATH = "/财务凭证调整(首付款或全额).csv";
-	public static final String CAIWUPINGZHENG_FILE_NAME = "/财务凭证调整(首付款或全额).csv";
+	public static final String CAIWUPINGZHENG_FILE_NAME = "财务凭证调整(首付款或全额).csv";//常规文件面无特殊字符
 	public static final String VBSBUSINESS_FILE_PATH = "/VBS业务号对应.csv";
-	public static final String VBSBUSINESS_FILE_NAME = "/VBS业务号对应.csv";
-
+	public static final String VBSBUSINESS_FILE_NAME = "VBS业务号对应.csv";
 	public static final String PAYMENTORFULLPAYMENT_FILE_PATH = "/首付款或全额（购买退货）流水.csv";
-	public static final String PAYMENTORFULLPAYMENT_FILE_NAME = "/首付款或全额（购买退货）流水.csv";
+	public static final String PAYMENTORFULLPAYMENT_FILE_NAME = "首付款或全额（购买退货）流水.csv";
 
 
 
