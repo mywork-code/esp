d440ade459e0c06d0d164b173a674b00fe010530
pengyingchao <pengyingchao@apass.cn>
2017-10-24 16:20:31 +0800

    invaid

--- esp-ams/src/main/java/com/apass/esp/web/offer/ProGroupGoodsExportFikeController.java
+++ esp-ams/src/main/java/com/apass/esp/web/offer/ProGroupGoodsExportFikeController.java
@@ -10,14 +10,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
