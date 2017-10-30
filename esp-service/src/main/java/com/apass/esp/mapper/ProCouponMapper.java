cdfffd87e1851597dd6043f05da13e7a93e5cb1f
zengqingshan <zengqingshan@apass.cn>
2017-10-30 13:40:19 +0800

    234

--- esp-service/src/main/java/com/apass/esp/mapper/ProCouponMapper.java
+++ esp-service/src/main/java/com/apass/esp/mapper/ProCouponMapper.java
@@ -1,15 +1,17 @@
 package com.apass.esp.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.apass.esp.domain.entity.ProCoupon;
 import com.apass.gfb.framework.mybatis.GenericMapper;
-import com.apass.gfb.framework.mybatis.page.Page;
 import com.apass.gfb.framework.mybatis.page.Pagination;
-
-import java.util.Map;
 
 /**
  * Created by jie.xu on 17/10/27.
  */
 public interface ProCouponMapper extends GenericMapper<ProCoupon, Long> {
     Pagination<ProCoupon> pageList(Map<String, Object> paramMap);
+    //
+    List<ProCoupon> getProCouponListByGoodsCode(String goodsCode);
 }
--- esp-service/src/main/java/com/apass/esp/service/offer/ProCouponService.java
+++ esp-service/src/main/java/com/apass/esp/service/offer/ProCouponService.java
@@ -1,14 +1,15 @@
 package com.apass.esp.service.offer;
 
-import com.apass.esp.domain.entity.ProCoupon;
-import com.apass.esp.mapper.ProCouponMapper;
-import com.apass.gfb.framework.mybatis.page.Pagination;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
+import com.apass.esp.domain.entity.ProCoupon;
+import com.apass.esp.mapper.ProCouponMapper;
+import com.apass.gfb.framework.mybatis.page.Pagination;
 
 /**
  * Created by xiaohai on 2017/10/30.
@@ -20,7 +21,10 @@ public class ProCouponService {
     private ProCouponMapper couponMapper;
 
     public Pagination<ProCoupon> pageList(Map<String, Object> paramMap) {
-        List<ProCoupon> proCouponList = couponMapper.pageList(paramMap);
-
+    	Pagination<ProCoupon> proCouponList = couponMapper.pageList(paramMap);
+        return proCouponList;
+    }
+    public List<ProCoupon> getProCouponListByGoodsCode(String goodsCode) {
+    	return couponMapper.getProCouponListByGoodsCode(goodsCode);
     }
 }
