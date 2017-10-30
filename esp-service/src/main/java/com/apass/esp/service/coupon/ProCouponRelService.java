8f1f74110aa0ad284e5b712181c7d4a14593dc5d
zengqingshan <zengqingshan@apass.cn>
2017-10-30 10:52:09 +0800

    优惠券12

---
+++ esp-service/src/main/java/com/apass/esp/service/coupon/ProCouponRelService.java
@@ -0,0 +1,11 @@
+package com.apass.esp.service.coupon;
+
+import org.springframework.stereotype.Component;
+/**
+ * 优惠券与活动关系Service
+ * @author aopai
+ */
+@Component
+public class ProCouponRelService {
+
+}
