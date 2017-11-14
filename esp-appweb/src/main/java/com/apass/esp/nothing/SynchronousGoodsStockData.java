f1d089ce9ea91671a62c847d3d1bd3cbb1b8cb0c
zengqingshan <zengqingshan@apass.cn>
2017-11-14 17:26:29 +0800

    同步已上架自己商户商品库存数据

--- esp-appweb/src/main/java/com/apass/esp/nothing/SynchronousGoodsStockData.java
+++ esp-appweb/src/main/java/com/apass/esp/nothing/SynchronousGoodsStockData.java
@@ -1,7 +1,10 @@
 package com.apass.esp.nothing;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -10,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.apass.esp.domain.Response;
 import com.apass.esp.domain.entity.GoodsAttr;
 import com.apass.esp.domain.entity.GoodsAttrVal;
 import com.apass.esp.domain.entity.goods.GoodsInfoEntity;
@@ -39,10 +44,17 @@ public class SynchronousGoodsStockData {
 	 * 
 	 * @return
 	 */
+	@ResponseBody
 	@RequestMapping(value = "/goodsStock", method = RequestMethod.GET)
-	public void SynchronousGoodsStock() {
+	public  Response SynchronousGoodsStock() {
+		Map<String,Object> map=new HashMap<>();
+		List<String> list1=new ArrayList<>();//所有非京东的商品的goodsid
+		List<String> list2=new ArrayList<>();//所有非京东的商品且skuid为空的goodsStockId
+		List<String> list3=new ArrayList<>();//所有非京东的商品的规格更新成功的goodsStockId
 		List<GoodsInfoEntity> goodsList = goodsService.getNotJDgoodsList();
+		map.put("非京东商品的数量为：", goodsList.size());
 		for (GoodsInfoEntity goodsInfoEntity : goodsList) {
+			list1.add(goodsInfoEntity.getId().toString());
 			GoodsAttr entity = new GoodsAttr();
 			if (StringUtils.isNotEmpty(goodsInfoEntity.getGoodsSkuType())) {
 				entity.setName(goodsInfoEntity.getGoodsSkuType());
@@ -63,6 +75,7 @@ public class SynchronousGoodsStockData {
 				Long GoodsAttrValId = null;
 				for (GoodsStockInfoEntity goodsStockInfoEntity : goodsStockInfoList) {
 					if (StringUtils.isBlank(goodsStockInfoEntity.getSkuId())) {//排除已经添加了的数据（非京东）
+						list2.add(goodsStockInfoEntity.getId().toString());
 						// 商品不同规格下对应值表插入数据并返回主键id
 						goodsAttrVal = new GoodsAttrVal();
 						List<GoodsAttrVal> list = goodsAttrValService.goodsAttrValList(attrId,
@@ -86,11 +99,16 @@ public class SynchronousGoodsStockData {
 							goodsStockInfo.setAttrValIds(GoodsAttrValId.toString());
 							goodsStockInfo.setUpdateUser("admin");
 							goodsStockDao.updateService(goodsStockInfo);
+							list3.add(goodsStockInfo.getId().toString());
 						}
 
 					}
 				}
 			}
 		}
+		map.put("所有非京东的商品的goodsid", list1);
+		map.put("所有非京东的商品且skuid为空的goodsStockId", list2);
+		map.put("所有非京东的商品的规格更新成功的goodsStockId", list3);
+		return Response.success("非京东商品规格同步成功！", map);
 	}
 }
