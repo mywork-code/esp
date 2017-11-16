d616d08dac54d0c56af3c0c76b5ab0badf724a27
zengqingshan <zengqingshan@apass.cn>
2017-11-16 10:11:42 +0800

    修改商品详情接口

--- esp-service/src/main/java/com/apass/esp/third/party/weizhi/client/WeiZhiProductDetailResponse.java
+++
@@ -1,7 +0,0 @@
-package com.apass.esp.third.party.weizhi.client;
-
-import com.apass.esp.third.party.jd.entity.product.Product;
-
-public class WeiZhiProductDetailResponse extends WeiZhiResponse<Product>{
-
-}
