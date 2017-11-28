7ef292b2a70c1de68c9c903ae697b8432d4e93e8
pengyingchao <pengyingchao@apass.cn>
2017-11-28 18:36:44 +0800

    invalid code

--- esp-appweb/src/main/java/com/apass/esp/nothing/SucceedOrderInfoController.java
+++ esp-appweb/src/main/java/com/apass/esp/nothing/SucceedOrderInfoController.java
@@ -1,23 +1,24 @@
 package com.apass.esp.nothing;
 
-import com.apass.esp.common.code.BusinessErrorCode;
-import com.apass.esp.domain.Response;
-import com.apass.esp.service.order.OrderService;
-import com.apass.gfb.framework.utils.BaseConstants.ParamsCode;
-import com.apass.gfb.framework.utils.CommonUtils;
-import com.apass.gfb.framework.utils.GsonUtils;
-import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.apass.esp.common.code.BusinessErrorCode;
+import com.apass.esp.domain.Response;
+import com.apass.esp.service.order.OrderService;
+import com.apass.gfb.framework.utils.BaseConstants.ParamsCode;
+import com.apass.gfb.framework.utils.CommonUtils;
+import com.google.common.collect.Maps;
 
 @Path("/order")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
