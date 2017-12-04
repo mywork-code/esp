af6e70e9706f9cab87c699c351dc1ad57b1f88d4
haotian <wanghaotian@apass.cn>
2017-12-04 15:45:42 +0800

    limitbuy

---
+++ esp-domain/src/main/java/com/apass/esp/domain/dto/activity/LimitBuyAct.java
@@ -0,0 +1,85 @@
+package com.apass.esp.domain.dto.activity;
+
+import java.util.Date;
+
+public class LimitBuyAct {
+    private Long id;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String status;
+
+    private String createUser;
+
+    private String updateUser;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
---
+++ esp-domain/src/main/java/com/apass/esp/domain/dto/activity/LimitBuydetail.java
@@ -0,0 +1,75 @@
+package com.apass.esp.domain.dto.activity;
+
+import java.util.Date;
+
+public class LimitBuydetail {
+    private Long id;
+
+    private Long limitBuyActId;
+
+    private Long limitGoodsSkuId;
+
+    private Long userId;
+
+    private Integer buyNo;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getLimitBuyActId() {
+        return limitBuyActId;
+    }
+
+    public void setLimitBuyActId(Long limitBuyActId) {
+        this.limitBuyActId = limitBuyActId;
+    }
+
+    public Long getLimitGoodsSkuId() {
+        return limitGoodsSkuId;
+    }
+
+    public void setLimitGoodsSkuId(Long limitGoodsSkuId) {
+        this.limitGoodsSkuId = limitGoodsSkuId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getBuyNo() {
+        return buyNo;
+    }
+
+    public void setBuyNo(Integer buyNo) {
+        this.buyNo = buyNo;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
