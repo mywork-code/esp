078d321efeea1770015a473f67048d9880f64cc4
pengyingchao <pengyingchao@apass.cn>
2017-12-11 16:14:07 +0800

    limitbuydetail

--- esp-service/src/main/java/com/apass/esp/service/activity/LimitCommonService.java
+++ esp-service/src/main/java/com/apass/esp/service/activity/LimitCommonService.java
@@ -4,13 +4,12 @@ import java.util.Date;
 import java.util.List;
 import java.util.TreeMap;
 
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.apass.esp.domain.entity.LimitBuyAct;
-import com.apass.esp.domain.entity.LimitBuydetail;
+import com.apass.esp.domain.entity.LimitBuyDetail;
 import com.apass.esp.domain.entity.LimitGoodsSku;
 import com.apass.esp.domain.entity.activity.LimitGoodsSkuVo;
 import com.apass.esp.domain.entity.goods.GoodsInfoEntity;
@@ -192,12 +191,12 @@ public class LimitCommonService {
 		/**
 		 * 根据限时购的活动ID和用户ID和skuID,查询某一用户在某一活动下，购买某一件商品的数量
 		 */
-		List<LimitBuydetail> buyDetails = buydetailMapper.getUserBuyGoodsNum(new LimitBuyParam(params.getUserId(), limitGoods.getId()+""));
+		List<LimitBuyDetail> buyDetails = buydetailMapper.getUserBuyGoodsNum(new LimitBuyParam(params.getUserId(), limitGoods.getId()+""));
 		/**
 		 * 计算用户购买了同一个活动同一商品的件数
 		 */
 		long goodsSum = 0l;
-		for (LimitBuydetail limitBuydetail : buyDetails) {
+		for (LimitBuyDetail limitBuydetail : buyDetails) {
 			goodsSum += limitBuydetail.getBuyNo();
 		}
 		goodsSum += params.getNum().longValue();
