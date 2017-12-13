26c3cc2f7454019edbedb650c066d8e8709c8ebe
zengqingshan <zengqingshan@apass.cn>
2017-12-13 09:56:56 +0800

    修改123

--- esp-service/src/main/java/com/apass/esp/third/party/weizhi/client/WeiZhiConstants.java
+++ esp-service/src/main/java/com/apass/esp/third/party/weizhi/client/WeiZhiConstants.java
@@ -30,18 +30,18 @@ public class WeiZhiConstants {
             clientSecret = "pm0G8ZzOQ2XItQnq9q1r";
             requestUrl = "http://openapi.viphrm.com";
         }else{
-            tokenUrl = "http://testopenapi.viphrm.com/jdapi/accessToken";
-            clientId = "100118132";
-            userName = "prezydc";
-            password = "124456";
-            clientSecret = "4U0pYYAbuhNNzXMvF3wy";
-            requestUrl = "http://testopenapi.viphrm.com";
 //            tokenUrl = "http://testopenapi.viphrm.com/jdapi/accessToken";
 //            clientId = "100118132";
 //            userName = "prezydc";
 //            password = "124456";
 //            clientSecret = "4U0pYYAbuhNNzXMvF3wy";
 //            requestUrl = "http://testopenapi.viphrm.com";
+            tokenUrl = "http://testopenapi.viphrm.com/jdapi/accessToken";
+            clientId = "100118132";
+            userName = "prezydc";
+            password = "124456";
+            clientSecret = "4U0pYYAbuhNNzXMvF3wy";
+            requestUrl = "http://testopenapi.viphrm.com";
         }
 
     }
