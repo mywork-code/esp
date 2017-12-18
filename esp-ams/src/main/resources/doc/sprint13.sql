2e20ec600d3441259881378d0889d2f90ba329e8
sunchaohai <sunchaohai@apass.cn>
2017-12-18 14:14:39 +0800

    sprint13.sql修改库存表商品规格长度

--- esp-ams/src/main/resources/doc/sprint13.sql
+++ esp-ams/src/main/resources/doc/sprint13.sql
@@ -63,3 +63,5 @@ alter table t_esp_goods_base_info add column main_goods_code varchar(20) not nul
 alter table t_esp_limit_goods_sku add up_load_status tinyint not null DEFAULT 1 COMMENT '商品上传成功标志 1：成功；0：失败    默认为1';
 ALTER TABLE `t_esp_limit_goods_sku`
 MODIFY COLUMN `url`  varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '限时购缩略图URL' AFTER `sort_no`;
+ALTER TABLE `t_esp_goods_stock_info`
+MODIFY COLUMN `goods_sku_attr`  varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品最小单元sku属性(商品规格)' AFTER `goods_id`;
