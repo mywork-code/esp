31a3edaed8d9806b9a8d3b50704c27313f29f655
xujie <xujie@apass.cn>
2018-01-25 15:52:43 +0800

    C:/Program Files/Git/DataEsporderAnalysisMapper

---
+++ esp-domain/src/main/java/com/apass/esp/domain/entity/DataEsporderAnalysis.java
@@ -0,0 +1,126 @@
+package com.apass.esp.domain.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class DataEsporderAnalysis {
+    private Long id;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    private String isDelete;
+
+    private String txnId;
+
+    private Integer confirmNum;
+
+    private Integer confirmGoodsNum;
+
+    private BigDecimal confirmAmt;
+
+    private Integer payNum;
+
+    private Integer payGoodsNum;
+
+    private BigDecimal payAmt;
+
+    private BigDecimal percentConv;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    public String getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(String isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public String getTxnId() {
+        return txnId;
+    }
+
+    public void setTxnId(String txnId) {
+        this.txnId = txnId;
+    }
+
+    public Integer getConfirmNum() {
+        return confirmNum;
+    }
+
+    public void setConfirmNum(Integer confirmNum) {
+        this.confirmNum = confirmNum;
+    }
+
+    public Integer getConfirmGoodsNum() {
+        return confirmGoodsNum;
+    }
+
+    public void setConfirmGoodsNum(Integer confirmGoodsNum) {
+        this.confirmGoodsNum = confirmGoodsNum;
+    }
+
+    public BigDecimal getConfirmAmt() {
+        return confirmAmt;
+    }
+
+    public void setConfirmAmt(BigDecimal confirmAmt) {
+        this.confirmAmt = confirmAmt;
+    }
+
+    public Integer getPayNum() {
+        return payNum;
+    }
+
+    public void setPayNum(Integer payNum) {
+        this.payNum = payNum;
+    }
+
+    public Integer getPayGoodsNum() {
+        return payGoodsNum;
+    }
+
+    public void setPayGoodsNum(Integer payGoodsNum) {
+        this.payGoodsNum = payGoodsNum;
+    }
+
+    public BigDecimal getPayAmt() {
+        return payAmt;
+    }
+
+    public void setPayAmt(BigDecimal payAmt) {
+        this.payAmt = payAmt;
+    }
+
+    public BigDecimal getPercentConv() {
+        return percentConv;
+    }
+
+    public void setPercentConv(BigDecimal percentConv) {
+        this.percentConv = percentConv;
+    }
+}
---
+++ esp-service/src/main/java/com/apass/esp/mapper/DataEsporderAnalysisMapper.java
@@ -0,0 +1,10 @@
+package com.apass.esp.mapper;
+
+import com.apass.esp.domain.entity.DataEsporderAnalysis;
+import com.apass.gfb.framework.mybatis.GenericMapper;
+
+/**
+ * Created by DELL on 2018/1/25.
+ */
+public interface DataEsporderAnalysisMapper  extends GenericMapper<DataEsporderAnalysis,Long> {
+}
