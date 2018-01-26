aabc5513bed8d7ff34a787dafc11b0122c43a772
haotian <wanghaotian@apass.cn>
2018-01-26 13:43:34 +0800

    运营分析

---
+++ esp-appweb/src/main/java/com/apass/esp/schedule/OrderAnalysisScheduleTask.java
@@ -0,0 +1,37 @@
+package com.apass.esp.schedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.apass.esp.service.dataanalysis.DataEsporderAnalysisService;
+/**
+ * 订单统计定时任务
+ * @author Administrator
+ */
+@Component
+@Configurable
+@EnableScheduling
+@Profile("Schedule")
+public class OrderAnalysisScheduleTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderAnalysisScheduleTask.class);
+    @Autowired
+    private DataEsporderAnalysisService dataEsporderAnalysisService;
+    /**
+     * 商城订单统计
+     * 每日10点执行，刷新昨日订单统计
+     * 针对 t_data_esporder_analysis 和  t_data_esporderdetail
+     */
+    @Scheduled(cron = "0 0 10 * * ?")
+    public void flushEsporderAnalysis(){
+        try {
+        	dataEsporderAnalysisService.flushEsporderAnalysis();
+        } catch (Exception e) {
+            LOGGER.error("商城订单统计出现异常", e);
+        }
+    }
+}
