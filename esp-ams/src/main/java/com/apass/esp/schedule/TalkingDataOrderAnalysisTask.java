80ea98422333ddfdd0397474ebcdfc3b603a0f20
haotian <wanghaotian@apass.cn>
2018-01-31 10:46:22 +0800

    订单统计定时任务

--- esp-ams/src/main/java/com/apass/esp/schedule/TalkingDataOrderAnalysisTask.java
+++ esp-ams/src/main/java/com/apass/esp/schedule/TalkingDataOrderAnalysisTask.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import com.apass.esp.service.dataanalysis.DataEsporderAnalysisService;
 /**
  * 订单统计定时任务
@@ -17,13 +16,13 @@ import com.apass.esp.service.dataanalysis.DataEsporderAnalysisService;
 @Configurable
 @EnableScheduling
 @Profile("Schedule")
-public class OrderAnalysisScheduleTask {
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderAnalysisScheduleTask.class);
+public class TalkingDataOrderAnalysisTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TalkingDataOrderAnalysisTask.class);
     @Autowired
     private DataEsporderAnalysisService dataEsporderAnalysisService;
     /**
      * 商城订单统计
-     * 每日10点执行，刷新昨日订单统计
+     * 每日2点执行，刷新昨日订单统计
      * 针对 t_data_esporder_analysis 和  t_data_esporderdetail
      */
     @Scheduled(cron = "0 0 2 * * ?")
