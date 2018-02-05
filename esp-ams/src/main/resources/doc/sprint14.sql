3940e61102c106bb0ff4abb5c0bde2e5a12c948d
xujie <xujie@apass.cn>
2018-02-05 11:16:19 +0800

    sql

--- esp-ams/src/main/resources/doc/sprint14.sql
+++ esp-ams/src/main/resources/doc/sprint14.sql
@@ -1,6 +1,4 @@
 use esp;
-ALTER TABLE `t_esp_bsdiff_info`
-ADD COLUMN `if_compel_update`  tinyint(2) NOT NULL DEFAULT 0 COMMENT '是否强制更新1：是，0：否';
 
 DROP TABLE t_data_appuser_analysis;
 CREATE TABLE `t_data_appuser_analysis` (
