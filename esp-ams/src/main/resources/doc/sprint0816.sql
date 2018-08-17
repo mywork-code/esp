975b4d9e7fa755d906a1b25e90c88a496a79cfd8
xujie <xujie@apass.cn>
2018-08-17 14:35:59 +0800

    sql

--- esp-ams/src/main/resources/doc/sprint0816.sql
+++ esp-ams/src/main/resources/doc/sprint0816.sql
@@ -1,6 +1,6 @@
 use esp;
 CREATE TABLE `t_esp_pa_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned  NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `user_id` bigint(20) NOT NULL DEFAULT '-1' COMMENT '用户id',
   `username` varchar(32) NOT NULL DEFAULT '' COMMENT '用户姓名',
   `birthday` date NOT NULL DEFAULT '1900-01-01' COMMENT '生日',
@@ -10,5 +10,6 @@ CREATE TABLE `t_esp_pa_user` (
   `updated_time` datetime NOT NULL DEFAULT '1900-01-01 00:00:00' COMMENT '修改时间',
   `is_delete` varchar(2) NOT NULL DEFAULT 'N' COMMENT '是否删除，Y：是；N：否',
   `from_ip` varchar(32) NOT NULL DEFAULT '' COMMENT '客户来源ip,请求的request中获取',
+   `identity` varchar(32) NOT NULL DEFAULT '' COMMENT '身份证',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户领取平安保险记录表';
