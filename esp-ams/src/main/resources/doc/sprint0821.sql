0ce3278a020c11fcd688390f2f6256f19b174411
xujie <xujie@apass.cn>
2018-08-24 15:58:11 +0800

    1

--- esp-ams/src/main/resources/doc/sprint0821.sql
+++ esp-ams/src/main/resources/doc/sprint0821.sql
@@ -1,6 +1,6 @@
 use esp;
 CREATE TABLE `t_esp_zy_prize_collec` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `created_time` datetime NOT NULL DEFAULT '1900-01-01 00:00:00' COMMENT '创建时间',
   `updated_time` datetime NOT NULL DEFAULT '1900-01-01 00:00:00' COMMENT '修改时间',
   `is_delete` varchar(2) NOT NULL DEFAULT 'N' COMMENT '是否删除，Y：是；N：否',
